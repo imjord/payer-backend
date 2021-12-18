fb37892a54a085beff3d8f87bb997124eae849d8
imjord <dean.jay.goat@gmail.com>
2021-12-17 19:15:56 -0700

    redid controller

--- controllers/PayerController.js
+++ controllers/PayerController.js
@@ -1,27 +1,30 @@
-// import library 
-const router = require('express').Router;
+
 
 // import the payer model
 const Payer = require('../models/Payer');
 
 
-// get payer
-
-router.get('/', (req,res) => {
-    Payer.findAll().then(data => {res.json(data)}).catch(err => res.status(500).json(err))
-})
-
-// add transaction 
-router.post('/', (req,res) => {
-    Payer.create({
-        payer: req.body.payer,
-        points: req.body.points
-    }).then(data => {
-        res.json(data);
-    }).catch(err => {
-        res.status(500).json(err);
-    })
-})
 
 
-module.exports = router;
+
+const payerController = {
+    //get payer
+    getPayer(req,res) {
+        Payer.findAll().then(data => {res.json(data)}).catch(err => res.status(500).json(err))
+
+    },
+    // create payer
+    createPayer(req,res){
+        Payer.create({
+            payer: req.body.payer,
+            points: req.body.points
+        }).then(data => {
+            res.json(data);
+        }).catch(err => {
+            res.status(500).json(err);
+        })
+    }
+    
+}
+
+
